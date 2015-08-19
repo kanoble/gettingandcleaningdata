d48a78e4d7c45820d4efdd075d581bb4b15bcda6
Kevin Noble <kanoble@gmail.com>
2015-08-18 23:55:12 -0500

    Expected final version of code that complies with the assignment parameters. Includes output txt file.

--- run_analysis.R
+++ run_analysis.R
@@ -1,31 +1,80 @@
-## http://archive.ics.uci.edu/ml/datasets/Human+Activity+Recognition+Using+Smartphones
-## Data source: https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip
+## Script for merging the test and train data sets in accordance with
+## instructions for Coursera's Getting and Cleaning Data course project.
+## The result of the script is a tidy data set that contains the means
+## (averages) of each subject for each activity.
+
+## Original data source: https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip
 
 ## Initialize libraries that contain dependent functions for the program.
 library(data.table)
+library(dplyr)
 
-## Read files
+## Read files for analysis.
 x_test <- read.table("UCI HAR Dataset/test/X_test.txt")
 y_test<- read.table("UCI HAR Dataset/test/y_test.txt")
+subject_test <- read.table("UCI HAR Dataset/test/subject_test.txt")
 x_train <- read.table("UCI HAR Dataset/train/X_train.txt")
 y_train <- read.table("UCI HAR Dataset/train/y_train.txt")
+subject_train <- read.table("UCI HAR Dataset/train/subject_train.txt")
 features <- read.table("UCI HAR Dataset/features.txt")
+activity_labels <- read.table("UCI HAR Dataset/activity_labels.txt")
 
-## 
-namevector <- as.vector(features[,2])
+## Create appropriate column names for the data frames.
+## The column names for the x_ data frames come from the features file.
+colnames(x_test) <- as.vector(features[,2])
+colnames(x_train) <- as.vector(features[,2])
+colnames(y_test) <- "Activity_id"
+colnames(y_train) <- "Activity_id"
+colnames(subject_test) <- "Subject"
+colnames(subject_train) <- "Subject"
+colnames(activity_labels) <- c("Activity_id", "Activity")
 
+## Merge the training and the test sets to create one data set.
+## Combine the subject_, y_, and x_ files for the test and train
+## data sets, then rbind those two large data frames together for the
+## final merged set.
+mergedset <- data.frame()
+x_test <- cbind(subject_test, y_test, x_test)
+x_train <- cbind(subject_train, y_train, x_train)
+mergedset <- rbind(x_test, x_train)
 
-## 1 - Merges the training and the test sets to create one data set.
-mergedFile <- data.frame()
-mergedFile <- rbind(x_test, x_train)
+## Remove variables (columns) from the merged data set that do not
+## include the strings "std," "mean," or "Mean" in their name.
+## The Activity and Subject columns are included in the grep command
+## as they're needed for the eventual summary.
+mergedset <- mergedset[,grepl("Activity|Subject|std|mean|Mean", names(mergedset))]
 
-## 2 - Extracts only the measurements on the mean and standard deviation for each measurement. 
+## Replace the activity ids (codes 1 through 6) with the natural 
+## language name of that activity as found in the activity_labels file.
+mergedset <- merge(activity_labels, mergedset, by = "Activity_id")
 
+## Remove the extraneous Activity_id column.
+mergedset <- mergedset[,-1]
 
-## 3 - Uses descriptive activity names to name the activities in the data set
+## Reorder the columns so that the Subject is first.
+mergedset <- select(mergedset, Subject, Activity, 3:ncol(mergedset))
 
+## Calculate the means (averages) of each subject for each activity.
+mergedset <- mergedset %>% 
+    group_by(Subject, Activity) %>% 
+    summarise_each(funs(mean))
 
-## 4 - Appropriately labels the data set with descriptive variable names. 
+## Other cleanup of column names to make them tidy.
+colnames(mergedset) <- sub("Acc", "Acceleration", colnames(mergedset), fixed = TRUE)
+colnames(mergedset) <- sub("Gyro", "Gyroscope", colnames(mergedset), fixed = TRUE)
+colnames(mergedset) <- sub("Mag", "Magnitude", colnames(mergedset), fixed = TRUE)
+colnames(mergedset) <- sub("BodyBody", "Body", colnames(mergedset), fixed = TRUE)
+colnames(mergedset) <- sub("Freq", "Frequency", colnames(mergedset), fixed = TRUE)
+colnames(mergedset) <- sub("\\(\\)", "", colnames(mergedset))
+#colnames(mergedset) <- sub("\\)", "", colnames(mergedset))
+#colnames(mergedset) <- sub(".", "", colnames(mergedset))
+#colnames(mergedset) <- sub("_", "", colnames(mergedset))
 
+## Revise the names of the column variables to indicate that they are 
+## now the mean of their original values.
+colnames(mergedset) <- paste("Mean of:", colnames(mergedset), sep = " ")
+colnames(mergedset)[1] <- "Subject"
+colnames(mergedset)[2] <- "Activity"
 
-## 5 - From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+## Create a text file of the final merged data set.
+write.table(mergedset, file = "mergedset.txt", row.name = FALSE)
